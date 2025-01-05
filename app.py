91e9acf08ce6efb188a3ab91298e8913188c6d44
Salma J <87875257+ssszlma@users.noreply.github.com>
2025-01-05 14:49:25 -0500

    update

--- app.py
+++ app.py
@@ -7,11 +7,10 @@ app = Flask(__name__)
 data = pd.read_csv('data.csv')
 
 data = data.dropna(subset=['ra', 'dec', 'z'])
-data['Distance'] = data['z'] * 3.0e5 / 70.0  # Hubble's Law: Distance = z * speed_of_light / Hubble_constant
+data['Distance'] = data['z'] * 3.0e5 / 70.0  # Distance = z * speed of light / Hubble const
 
 @app.route('/')
 def index():
-    # Create the 3D plot using Plotly with always visible modebar
     fig = go.Figure(data=[go.Scatter3d(
         x=data['ra'],
         y=data['dec'],
